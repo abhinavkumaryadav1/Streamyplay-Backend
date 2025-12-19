877fd56caded16538e737ba2f58e8f50315853a5
Abhinav Kumar Yadav <abhinavk1608@gmail.com>
2025-12-19 22:01:05 +0530

    removed authontication middleware for video

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -11,12 +11,12 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+// router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router
     .route("/")
     .get(getAllVideos)
-    .post(
+    .post(verifyJWT,
         upload.fields([
             {
                 name: "videoFile",
@@ -33,9 +33,9 @@ router
 
 router
     .route("/:videoId")
-    .get(getVideoById)
-    .delete(deleteVideo)
-    .patch(upload.single("thumbnail"), updateVideo);
+    .get(verifyJWT,getVideoById)
+    .delete(verifyJWT,deleteVideo)
+    .patch(verifyJWT,upload.single("thumbnail"), updateVideo);
 
 router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
 
